5a10b80d7569b7e8c1ce2063c4bc96d843c9b71e
Bhavagna Pinninti <bhavagnap@infinite.com>
2023-09-25 13:29:08 +0530

    my git

---
+++ Code_BaseTest_Csharp/Code_BaseTest3/codebasetest3/codebasetest3/Box.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codebasetest3
+{
+    using System;
+
+
+    class Box
+    {
+        public double Length { get; set; }
+        public double Breadth { get; set; }
+
+        public Box(double length, double breadth)
+        {
+            Length = length;
+            Breadth = breadth;
+        }
+
+        public static Box Add(Box box1, Box box2)
+        {
+            double combinedLength = box1.Length + box2.Length;
+            double combinedBreadth = box1.Breadth + box2.Breadth;
+            return new Box(combinedLength, combinedBreadth);
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Length: {Length}");
+            Console.WriteLine($"Breadth: {Breadth}");
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Box box1 = new Box(50, 30);
+            Box box2 = new Box(20, 40);
+
+            Box box3 = Box.Add(box1, box2);
+
+            Console.WriteLine("Box 1:");
+            box1.DisplayDetails();
+
+            Console.WriteLine("\nBox 2:");
+            box2.DisplayDetails();
+
+            Console.WriteLine("\nBox 3:");
+            box3.DisplayDetails();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Code_BaseTest_Csharp/Code_BaseTest3/codebasetest3/codebasetest3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codebasetest3 
+{
+    using System;
+
+    class Cricket
+    {
+        public void Pointscalculation(int[] no_of_matches)
+        {
+            int sum = 0;
+
+            foreach (int score in no_of_matches)
+            {
+                sum += score;
+            }
+
+            double average = (double)sum / no_of_matches.Length;
+
+            Console.WriteLine($"Sum of scores: {sum}");
+            Console.WriteLine($"Average score: {average}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of matches: ");
+            if (int.TryParse(Console.ReadLine(), out int no_of_matches) && no_of_matches > 0)
+            {
+                int[] scores = new int[no_of_matches];
+
+                for (int i = 0; i < no_of_matches; i++)
+                {
+                    Console.Write($"Enter score for match {i + 1}: ");
+                    if (int.TryParse(Console.ReadLine(), out int score))
+                    {
+                        scores[i] = score;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input,enter a valid score.");
+                        i--; 
+                    }
+                }
+
+                Cricket cricket = new Cricket();
+                cricket.Pointscalculation(scores);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, enter a valid number of matches.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
