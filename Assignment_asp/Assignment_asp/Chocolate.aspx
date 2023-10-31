f6cd0931388d11f89835e59e12f7f0a37524129a
Bhavagna Pinninti <bhavagnap@infinite.com>
2023-10-31 17:54:18 +0530

    mygit

---
+++ Assignment_asp/Assignment_asp/Chocolate.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Chocolate.aspx.cs" Inherits="Assignment_asp.Chocolate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+   <form id="form1" runat="server">
+        <div>
+            <h1> Chocolate:</h1>
+            <asp:DropDownList ID="ddlItems" runat="server" AutoPostBack="true" OnSelectedIndexChanged="ddlItems_SelectedIndexChanged">
+                <asp:ListItem Text="Choose a chocolate" Value="0" />
+                <asp:ListItem Text="Dairy Milk" Value="1"  />
+                <asp:ListItem Text="5 Star" Value="2"  />
+                <asp:ListItem Text="Kit Kat" Value="3"  />
+                <asp:ListItem Text="Snickers" Value="4"  />
+
+            </asp:DropDownList>
+            <br /><br />
+
+            <asp:Image ID="imgItem"  runat="server"  />
+
+            <br /><br />
+
+            <asp:Button ID="btnShowCost" runat="server" Text="Show Cost" OnClick="btnShowCost_Click" />
+            <br /><br />
+
+            <asp:Label ID="lblCost" runat="server" Text="" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Assignment_asp/Assignment_asp/Chocolate.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment_asp
+{
+    public partial class Chocolate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+        }
+        protected void btnShowCost_Click(object sender, EventArgs e)
+        {
+            string selectedItem = ddlItems.SelectedValue;
+            string cost = string.Empty;
+            if (selectedItem == "1")
+            {
+                cost = "100";
+            }
+            else if (selectedItem == "2")
+            {
+                cost = "20";
+            }
+            else if (selectedItem == "3")
+            {
+                cost = "30";
+            }
+            else if (selectedItem == "4")
+            {
+                cost = "40";
+            }
+            string Item = ddlItems.SelectedItem.Text;
+            lblCost.Text = $"Cost of {Item}: {cost}";
+
+        }
+        protected void ddlItems_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedItem = ddlItems.SelectedValue;
+            string cost = string.Empty;
+            if (selectedItem == "1")
+            {
+                imgItem.ImageUrl = $"Images/dairymilk.jpg";
+            }
+            else if (selectedItem == "2")
+            {
+                imgItem.ImageUrl = $"Images/5star.jpg";
+            }
+            else if (selectedItem == "3")
+            {
+                imgItem.ImageUrl = $"Images/kitkat.jpg";
+            }
+            else if (selectedItem == "4")
+            {
+                imgItem.ImageUrl = $"Images/snickers.jpg";
+            }
+            else
+            {
+               
+            }
+        }
+    }
+}
