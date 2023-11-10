32b2cfbcdb6f931c8a70c925a82dc944a4a86569
Bhavagna Pinninti <bhavagnap@infinite.com>
2023-11-10 17:49:32 +0530

    my git:

---
+++ Code_BaseTest_8/codebasetest8/codebasetest8/Controllers/code_controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace codebasetest8.Controllers
+{
+    public class code_controller : Controller
+    {
+        // GET: code_controller
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Code_BaseTest_8/codetest8/codetest8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="codetest8.MvcApplication" Language="C#" %>
