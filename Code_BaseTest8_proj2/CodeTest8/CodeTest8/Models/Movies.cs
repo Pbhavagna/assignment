480cf492a09e1d4039f8fc115164347e7a19b51a
Bhavagna Pinninti <bhavagnap@infinite.com>
2023-11-11 15:30:38 +0530

    mygit

---
+++ Code_BaseTest8_proj2/CodeTest8/CodeTest8/Models/Movies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeTest8.Models
+{
+    public class Movies
+    {
+        public int Mid { get; set; }
+        public string MovieName { get; set; }
+        public DateTime DateofRelease { get; set; }
+    }
+}
---
+++ Code_BaseTest8_proj2/Test-8/Test-8/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Test_8.MvcApplication" Language="VB" %>
